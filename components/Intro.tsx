393518713729ffd25e0165faf3131e88fc8df9fc
RuntimeTerrorrr <mubeen.rehman120@gmail.com>
2023-12-29 12:26:25 +0500

    feat: add parallex scrolling animation

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -1,9 +1,32 @@
 import Image from "next/image";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { useLayoutEffect, useRef } from "react";
 
 const Intro = () => {
+  const backgroundImage = useRef(null);
+  const introImage = useRef(null);
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    const timeline = gsap.timeline({
+      scrollTrigger: {
+        trigger: document.documentElement,
+        start: 0,
+        end: "+=500px",
+        scrub: true,
+        markers: true,
+      },
+    });
+
+    timeline
+      .from(backgroundImage.current, { clipPath: "inset(15%)" })
+      .to(introImage.current, { height: "200px" }, 0);
+  }, []);
+
   return (
     <div className="intro">
-      <div className="backgroundImage">
+      <div ref={backgroundImage} className="backgroundImage">
         <Image
           src="/images/background-one.jpg"
           alt="Background image"
@@ -13,11 +36,22 @@ const Intro = () => {
       </div>
 
       <div className="introContainer">
-        <div data-scroll data-scroll-speed='0.3' className="introImage">
-          <Image src="/images/background-two.jpg" alt="Background image" fill />
+        <div
+          ref={introImage}
+          data-scroll
+          data-scroll-speed="0.3"
+          className="introImage"
+        >
+          <Image
+            src="/images/background-two.jpg"
+            alt="Background image"
+            fill
+            className="object-cover object-top"
+          />
         </div>
-
-        <h1 data-scroll data-scroll-speed='0.7'>Smooth Scroll</h1>
+        <h1 data-scroll data-scroll-speed="0.7">
+          Happy Birthday
+        </h1>
       </div>
     </div>
   );
